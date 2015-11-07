557054f6e4e05462cd9f7e93c6f9ef255fb66d4b
padraigoleary <padraigoleary@gmail.com>
2015-11-07 19:58:25 +1030

    JSON API with Devise working

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
+
+#  devise_for :users
+
+
+
+  resources :todo_lists do
+    resources :todo_items do
+    	member do
+    		patch :complete
+    	end
+    end
+  end
+
+  root "todo_lists#index"
+end
